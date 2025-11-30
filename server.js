58242e92667e361821655032c1cac5a02c40948a
Prasanna venkatesh <168177926+14-Prasanna@users.noreply.github.com>
2025-11-30 21:08:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://14-prasanna.github.io/Mini-Audit-Trail-Generator-front-end/",
     credentials: true,
   })
 );
